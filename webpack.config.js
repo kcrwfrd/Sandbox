23deb2792e577f71351c6c5fbe42915f55c427d1
Kevin Crawford <kvcrawford@gmail.com>
2016-03-21 16:17:53 -0700

    chore(*): init app/webpack config

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+var path = require('path');
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  context: path.resolve(__dirname, 'src'),
+  entry: {
+    app: [
+      'webpack-dev-server/client?http://localhost:4000',
+      'webpack/hot/dev-server',
+      './index'
+    ],
+    vendor: [
+      'lodash'
+    ]
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/'
+  },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin('vendor', 'vendor.js'),
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src/index.html'),
+      hash: false,
+      filename: 'index.html',
+      inject: 'body',
+      minify: false
+    })
+  ],
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      loader: 'react-hot',
+      exclude: /node_modules/,
+      include: path.resolve(__dirname, 'src')
+    },{
+      test: /\.js$/,
+      loader: 'babel',
+      exclude: /node_modules/,
+      include: path.resolve(__dirname, 'src'),
+      query: {
+        cacheDirectory: true,
+        presets: ['es2015', 'react']
+      }
+    }, {
+      test: /\.css$/, // Only .css files
+      loader: 'style!css' // Run both loaders
+    }]
+  }
+};
