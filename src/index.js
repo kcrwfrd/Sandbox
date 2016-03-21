04039e605b88427095e337fb08a394a81598e457
Kevin Crawford <kvcrawford@gmail.com>
2016-03-21 16:19:28 -0700

    feat(*): init project source

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import './css/styles.css'
+
+ReactDOM.render(
+  <div className='container'>
+    <div className='row'>
+      <div className='col-md-6 col-md-offset-3'>
+        <h1>Sandbox</h1>
+      </div>
+    </div>
+  </div>,
+  document.getElementById('root')
+)
